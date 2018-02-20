9d897a527fba6e5f3aadcc287c59b0a69b0f686a
Peter Huang <peter.huang@farferch.com>
2018-02-20 19:31:20 +0800

    add picture, update layout

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+
+  resources :pictures
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
